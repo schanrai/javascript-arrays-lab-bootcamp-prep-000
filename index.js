3b92fda6a4a5c659fbb764c5c32ba2ce3929279f
Sushi Chanrai <sushi@pathworks.co>
2018-06-07 09:10:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function destructivelyAppendKitten(name){
 }
 
 
-/*function destructivelyPrependKitten(name){
+function destructivelyPrependKitten(name){
   kittens.unshift(name);
   return kittens;
 }*/
